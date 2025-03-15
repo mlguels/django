af6f37c3a595cce464cce841694d2027daae7d2f
Miguel Rodriguez <104812366+mlguels@users.noreply.github.com>
2025-03-14 18:08:20 -0700

    created a /files route

--- projectname/urls.py
+++ projectname/urls.py
@@ -16,7 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from projectname import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.home, name='home'),
+    path('files/', views.files, name='files')
 ]
+
