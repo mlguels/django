d089ae447c9ada3f93b5daf22a1d9e1f911d1797
Miguel Rodriguez <104812366+mlguels@users.noreply.github.com>
2025-03-16 16:16:51 -0700

    Misc + data update

--- projectname/views.py
+++ projectname/views.py
@@ -5,9 +5,10 @@ def home(request):
     return HttpResponse('Home page!')
 
 data = [
-    {'name': 'image1.jpeg', 'type': 'jpeg'},
-    {'name': 'notes.txt', 'type': 'txt'},
-    {'name': 'image2.jpeg', 'type': 'jpeg'}
+    {'id': 0, 'name': 'image1.jpeg', 'type': 'jpeg'},
+    {'id': 1, 'name': 'notes.txt', 'type': 'txt'},
+    {'id': 2, 'name': 'image2.jpeg', 'type': 'jpeg'}
 ]
+
 def files(request):
     return render(request, 'files/files.html', {'files': data})
