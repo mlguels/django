71937bff5538315029cfd230a6a18a3171d31517
Miguel Rodriguez <104812366+mlguels@users.noreply.github.com>
2025-03-22 14:54:05 -0700

    Removed mock data

--- projectname/views.py
+++ projectname/views.py
@@ -1,21 +1,18 @@
 from django.http import HttpResponse ,Http404
 from django.shortcuts import render
+from .models import File
 
 def home(request):
     return HttpResponse('Home page!')
 
-data = [
-    {'id': 0, 'name': 'image1.jpeg', 'type': 'jpeg'},
-    {'id': 1, 'name': 'notes.txt', 'type': 'txt'},
-    {'id': 2, 'name': 'image2.jpeg', 'type': 'jpeg'}
-]
 
 def files(request):
+    data = File.objects.all()
     return render(request, 'files/files.html', {'files': data})
-
+        
 def file(request, file_id):
-    f = next((item for item in data if item['id'] == file_id), None)
+    f = File.objects.get(pk=file_id)
     if f is not None:
         return render(request, 'files/file.html', {'file': f})
     else:
-        raise Http404("file does not exist")    
+        raise Http404("File does not exist")
