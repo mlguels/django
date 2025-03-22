92a950aba75bc7c1cd2e519f02467704cbac07c8
Miguel Rodriguez <104812366+mlguels@users.noreply.github.com>
2025-03-22 15:40:51 -0700

    Database updates

--- projectname/models.py
+++ projectname/models.py
@@ -1,8 +1,8 @@
 from django.db import models
 class File(models.Model):
     name = models.CharField(max_length=1024)
-    file_type = models.CharField(max_length=1024)
-    upload_timestamp = models.IntegerField(default=0)
+    file_type = models.CharField(max_length=10)
+    upload_timestamp = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.name
