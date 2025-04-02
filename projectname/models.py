162196afd901ea2df45785027620a87331474b06
Miguel Rodriguez <104812366+mlguels@users.noreply.github.com>
2025-04-01 20:01:23 -0700

    Add ability to upload files on server

--- projectname/models.py
+++ projectname/models.py
@@ -1,9 +1,9 @@
 from django.db import models
+
 class File(models.Model):
     name = models.CharField(max_length=1024)
-    file_type = models.CharField(max_length=10)
     upload_timestamp = models.DateTimeField(auto_now_add=True)
-
+    file = models.FileField()
+    
     def __str__(self):
         return self.name
-  
