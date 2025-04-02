0c90606b6e321867a9f20f856fda927f5f261048
Miguel Rodriguez <104812366+mlguels@users.noreply.github.com>
2025-04-01 20:19:44 -0700

    Created upload files path

--- projectname/urls.py
+++ projectname/urls.py
@@ -17,6 +17,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from projectname import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -24,6 +26,7 @@ urlpatterns = [
     path('content/files/', views.files, name='files'),
     path('file/<int:file_id>/', views.file, name='file'),
     path('files/edit/<int:file_id>/', views.edit, name='edit'),
-    path('files/delete/<int:file_id>/', views.delete, name='delete')
-]
+    path('files/delete/<int:file_id>/', views.delete, name='delete'),
+    path('files/upload/', views.upload, name='upload')
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
