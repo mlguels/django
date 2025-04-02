1686c7f7f25232c0d9b13388baafc924c772d6c4
Miguel Rodriguez <104812366+mlguels@users.noreply.github.com>
2025-04-01 20:19:32 -0700

    Add upload form functionality

---
+++ projectname/forms.py
@@ -0,0 +1,12 @@
+from django import forms
+from django.forms import ModelForm
+from .models import File
+
+class UploadForm(ModelForm):
+    file = forms.FileField()
+    class Meta:
+        model = File
+        fields = ['name', 'file']
+        widgets = {
+            'name': forms.TextInput(attrs={'placeholder': 'filename'})
+        }
