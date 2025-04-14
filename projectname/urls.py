40edd4b2f41a6e2d5e11aec90c6018739100ba0e
Miguel Rodriguez <104812366+mlguels@users.noreply.github.com>
2025-04-13 22:04:14 -0700

    Created dynamic url for file

--- projectname/urls.py
+++ projectname/urls.py
@@ -19,14 +19,12 @@ from django.urls import path
 from projectname import views
 from django.conf import settings
 from django.conf.urls.static import static
+from rest_framework.urlpatterns import format_suffix_patterns
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
-    path('content/files/', views.files, name='files'),
-    path('file/<int:file_id>/', views.file, name='file'),
-    path('files/edit/<int:file_id>/', views.edit, name='edit'),
-    path('files/delete/<int:file_id>/', views.delete, name='delete'),
-    path('files/upload/', views.upload, name='upload')
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    path('api/files/<int:file_id>/', views.file, name='file')
+]
 
+urlpatterns = format_suffix_patterns(urlpatterns)
