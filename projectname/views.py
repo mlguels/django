82bf73e47d5751d3b982006993eee823287d8172
Miguel Rodriguez <104812366+mlguels@users.noreply.github.com>
2025-04-13 23:37:09 -0700

    Add method for uploading files

--- projectname/views.py
+++ projectname/views.py
@@ -6,20 +6,40 @@ from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from rest_framework import status
 
-
-def files(request):
-    f = File.objects.all()
-    serializer = FileSerializer(f, many=True)
-    return JsonResponse({'files': serializer.data})
+@api_view(['GET', 'POST'])
+def files(request, format=None):
+    if request.method == 'GET':
+        data = File.objects.all()
+        serializer = FileSerializer(data, many=True)
+        return Response({'files': serializer.data})
+    
+    elif request.method == 'POST':
+        serializer = FileSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 def home(request):
     return HttpResponse("Hello there")
 
-@api_view(['GET'])
+@api_view(['GET', 'PATCH', 'DELETE'])
 def file(request, file_id, format=None):
     try:
         f = File.objects.get(pk=file_id)
     except File.DoesNotExist:
         return Response(status=status.HTTP_404_NOT_FOUND)
-    serializer = FileSerializer(f)
-    return JsonResponse({'file': serializer.data})
+    if request.method == 'GET':
+        serializer = FileSerializer(f)
+        return Response({'file': serializer.data}, status=status.HTTP_200_OK)
+    
+    elif request.method == 'PATCH':
+        serializer = FileSerializer(f, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    elif request.method == 'DELETE':
+        f.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
