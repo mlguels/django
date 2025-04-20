a89221004fcd43bd8a9935f90e5e0c7f685e0b61
Miguel Rodriguez <104812366+mlguels@users.noreply.github.com>
2025-04-20 16:17:17 -0700

    Add register path

--- projectname/urls.py
+++ projectname/urls.py
@@ -4,12 +4,17 @@ from projectname import views
 from django.conf import settings
 from django.conf.urls.static import static
 from rest_framework.urlpatterns import format_suffix_patterns
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
     path('api/files/', views.files, name='files'),
-    path('api/files/<int:file_id>/', views.file, name='file')
+    path('api/files/<int:file_id>/', views.file, name='file'),
+    path('api/auth/login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/auth/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/auth/register/', views.register, name='register'),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
