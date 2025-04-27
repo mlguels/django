3833b4c5b522c2d2aa08bcab59b7b07ccb9cf110
Miguel Rodriguez <104812366+mlguels@users.noreply.github.com>
2025-04-27 15:31:49 -0700

    Small updates

--- projectname/views.py
+++ projectname/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse, JsonResponse
 from .models import File
 from .serializers import FileSerializer
+from .serializers import FileSerializer, UserSerializer
 
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
@@ -8,6 +9,7 @@ from rest_framework import status
 
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated
+
 
 
 @api_view(['GET', 'POST'])
@@ -49,3 +51,10 @@ def file(request, file_id, format=None):
     elif request.method == 'DELETE':
         f.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
+    
+@api_view(['POST'])
+def register(request):
+    serializer = UserSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(status=status.HTTP_201_CREATED)
